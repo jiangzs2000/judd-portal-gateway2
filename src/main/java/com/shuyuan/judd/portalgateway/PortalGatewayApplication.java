8f387ab34b32be330e9483bc12f6c045b2a208ee
member1 <jiangzs2003@163.com>
2020-10-17 14:49:58 +0800

    migrate from eureka to nacos

--- src/main/java/com/shuyuan/judd/portalgateway/PortalGatewayApplication.java
+++ src/main/java/com/shuyuan/judd/portalgateway/PortalGatewayApplication.java
@@ -3,11 +3,11 @@ package com.shuyuan.judd.portalgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class PortalGatewayApplication {
 
